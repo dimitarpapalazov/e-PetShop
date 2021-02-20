504ac0c1a058fcfb523007f04fa91bc85fc96f86
Dimitar Papalazov <d.papalazov@outlook.com>
2021-02-20 14:37:41 +0100

    Change Member

--- web/src/main/java/mk/ukim/finki/wp/project/epetshop/demo/model/Member.java
+++ web/src/main/java/mk/ukim/finki/wp/project/epetshop/demo/model/Member.java
@@ -3,15 +3,18 @@ package mk.ukim.finki.wp.project.epetshop.demo.model;
 import lombok.Data;
 import mk.ukim.finki.wp.project.epetshop.demo.model.enumerations.MemberRole;
 import mk.ukim.finki.wp.project.epetshop.demo.model.enumerations.VerificationStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import java.util.Collection;
 
 @Data
 @Entity
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     private String username;
@@ -41,5 +44,30 @@ public class Member {
         this.lastName=lastName;
         this.role=role;
         this.status=status;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
